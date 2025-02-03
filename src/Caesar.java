e1d88b5843f8486e70fef24d69a5f65279aaf1ac
rnicolasp <rnicolasp@esliceu.net>
2025-02-03 19:19:20 +0100

    Caesar fet això és increíble

--- src/Caesar.java
+++ src/Caesar.java
@@ -1,14 +1,44 @@
 
 public class Caesar {
     static String cypher(String s, int delta) {
-        return "";
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isLetter(c)) {
+                char base = Character.isUpperCase(c) ? 'A' : 'a';
+                c = (char) ((c - base + delta) % 26 + base);
+            }
+            result.append(c);
+        }
+        return result.toString().toUpperCase();
     }
 
     static String decypher(String s, int delta) {
-        return "";
+        return cypher(s, -delta);
     }
 
     static String magic(String s, String ss) {
-        return "";
+
+        String[] mostCommonWords = {"el", "la", "de", "que", "i", "a", "en"};
+        int bestDelta = 0;
+        int highestMatchCount = 0;
+
+        for (int delta = 1; delta < 26; delta++) {
+            String decoded = decypher(s, delta);
+            int matchCount = 0;
+            for (String word : mostCommonWords) {
+                if (decoded.contains(word)) {
+                    matchCount++;
+                }
+            }
+
+            if (matchCount > highestMatchCount) {
+                bestDelta = delta;
+                highestMatchCount = matchCount;
+            }
+        }
+
+        return decypher(s, bestDelta);
+
     }
 }
